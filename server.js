926e2490a666e641ed825bd06fad7bb3439cdecc
oliverflood <oliveroldrich@gmail.com>
2022-12-01 17:31:36 -0800

    Created a general framework to get started Html, css, and js files link together, server runs with nodemon (I highly recommend you use it-> nodemon server.js). In test.js there is a small js game I found online to test out linking to a js file.

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
+
+
+
 var express = require("express")
+var exphbs = require('express-handlebars');
+
 
 var app = express()
-var port = 3000
+var port = process.env.PORT || 3000;
 
-app.get("/about", function (req, res, next) {
-    console.log("== Request received")
-    console.log("  -- req.url:", req.url)
-    console.log("  -- req.method:", req.method)
-    res.status(200).sendFile(__dirname + "/public/about.html")
-})
+// app.engine('handlebars', exphbs.engine({
+//   defaultLayout: "main"
+// }))
+// app.engine('handlebars')
+// app.set('view engine', 'handlebars')
+app.use(express.static("public"))
 
-app.get("*", function (req, res, next) {
-    console.log("== Request received")
-    console.log("  -- req.url:", req.url)
-    console.log("  -- req.method:", req.method)
-    res.status(404).sendFile(__dirname + "/public/index.html")
-})
+// app.get("/about", function (req, res, next) {
+//     console.log("== Request received")
+//     console.log("  -- req.url:", req.url)
+//     console.log("  -- req.method:", req.method)
+//     res.status(200).sendFile(__dirname + "/public/about.html")
+// })
+
+// app.get("*", function (req, res, next) {
+//     console.log("== Request received")
+//     console.log("  -- req.url:", req.url)
+//     console.log("  -- req.method:", req.method)
+//     res.status(404).sendFile(__dirname + "/public/index.html")
+// })
+
+
+
+
+
+// app.get('/', function (req, res, next) {
+//   res.status(200).render('', {})
+// })
+
+// app.get("*", function (req, res, next) {
+//   res.status(404).render('404')
+// });
+
 
 app.listen(port, function () {
     console.log("== Server is listening on port:", port)
