6ccd18d258f4ecc4125f2b1b0902885f1fa1169b
Pavlo Havrylyuk <12107961+Ghostbomb@users.noreply.github.com>
2022-12-06 18:39:13 -0800

    Better random function

--- public/index.js
+++ public/index.js
@@ -91,7 +91,7 @@ var startCast = false;
 var gameIsRunning = false;
 
 function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
